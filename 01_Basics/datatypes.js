ae8fd8da9221c439bb39ac8d48b561a6d0d77a21
Muhammad Owais <gamingcobra339@gmail.com>
2026-05-02 07:21:34 +0000

    some additions done

--- 01_Basics/datatypes.js
+++ 01_Basics/datatypes.js
@@ -4,4 +4,41 @@ let isLarge = true
 let temp = null
 
 
-console.log(typeof temp)
+// console.log(typeof temp)
+
+// Primitive
+// Number => Number
+// BigInt => BigInt
+// Boolean => Boolean
+// Null => Object
+// undefined => undefined
+// Symbol => Symbol
+// String => String
+
+// Non-Primitive
+// Array => Object
+// Function => Function(Object Function)
+// Object => Object
+
+// +++++++++++++++++++++++++++++++++++++++++++++
+
+//   Stack(Primitive) ..... Heap(Non-Primitive)
+
+let myname = "Muhammad Owais"
+let newname = myname
+
+newname = "Shoaib"
+
+console.log(myname)
+console.log(newname)
+
+let userone = {
+    name:"owais",
+    email: "134@gmail.com"
+}
+
+let usertwo = userone
+usertwo.email = "3399@gmail.com"
+
+console.log(userone.email)
+console.log(usertwo.email)
