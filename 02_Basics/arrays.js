2d2f8dfd1681bf68b34b1fbfc9a3be74bd6843ea
Muhammad Owais <gamingcobra339@gmail.com>
2026-05-02 19:30:46 +0000

    new

---
+++ 02_Basics/arrays.js
@@ -0,0 +1,12 @@
+const myarr = [0, 1, 2, 3 ,6 ,4]
+
+// myarr.push(49)
+// myarr.pop()
+// myarr.unshift(33)
+// myarr.shift(33)
+
+// console.log(myarr.includes(6))
+// console.log(myarr.indexOf(4))
+
+console.log(myarr)
+
