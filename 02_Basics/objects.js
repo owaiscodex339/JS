52725046b5b8fc1875fbe137af027ff4579e685f
Muhammad Owais <gamingcobra339@gmail.com>
2026-05-03 19:07:05 +0000

    new file

---
+++ 02_Basics/objects.js
@@ -0,0 +1,12 @@
+let User = {
+    name : "Owais",
+    "full name" : "Muhammad Owais",
+    age : 13,
+    location: "Mianwali",
+    email: "344@gmail.com",
+    isLoggedin: false,
+    lastLogindays: ["saturday", "sunday"]
+}
+console.log(User.email)
+console.log(User["email"])
+console.log(User["full name"])
