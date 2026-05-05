984a21719fd6e12e28417a196e914b31ab269021
Muhammad Owais <gamingcobra339@gmail.com>
2026-05-05 16:26:10 +0000

    new lines added

--- 02_Basics/objects.js
+++ 02_Basics/objects.js
@@ -1,6 +1,9 @@
+const mySym = Symbol("Key1")
+
 let User = {
     name : "Owais",
     "full name" : "Muhammad Owais",
+    [mySym]: "mykey1",
     age : 13,
     location: "Mianwali",
     email: "344@gmail.com",
@@ -10,3 +13,20 @@ let User = {
 console.log(User.email)
 console.log(User["email"])
 console.log(User["full name"])
+console.log(User[mySym])
+
+User.age = 34
+Object.freeze(User)
+User.age = 42
+console.log(User)
+
+User.greeting = function(){
+    console.log("Assalamoalaikum")
+}
+
+User.greetingTwo = function(){
+    console.log(`Hello from ${this.name}`)
+}
+
+console.log(User.greeting())
+console.log(User.greetingTwo())
