1217b3d66ca8cb1301881eec3eb6cca5ae47e42a
Muhammad Owais <gamingcobra339@gmail.com>
2026-05-12 19:22:42 +0000

    new files added

---
+++ 05_iteration/one.js
@@ -0,0 +1,9 @@
+for(let i = 1; i<=10; i++){
+    console.log("OuterLoop "+ i)
+    for(let j = 1; j <= 10; j++){
+        console.log(i+"*"+j+" = "+i*j)
+    }
+}
+
+// break: by using break the loop stops
+//continue : by using continue the loop continues
