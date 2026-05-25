537e7c0ad721e1fb5bab2ab073acc19be9832186
owaiscodex339 <gamingcobra339@gmail.com>
2026-05-25 23:20:13 +0500

    changes made

--- 10_OOP/Prototypes.js
+++ 10_OOP/Prototypes.js
@@ -1,6 +1,7 @@
 // let myName = "Owais     "
 // console.log(myName.trueLength)
 
+
 let myHeros = ['thor', 'spiderman']
 
 let heroPower = {
@@ -12,4 +13,55 @@ let heroPower = {
     }
 }
 
-heroPower.owais()
+Object.prototype.owais = function(){
+    console.log(`Owais is present in all objects`)
+}
+
+Array.prototype.heyOwais = function(){
+    console.log("Owais says hello")
+}
+
+
+// heroPower.owais()
+// myHeros.owais()
+// myHeros.heyOwais()
+// heroPower.heyOwais()
+
+//inheritence
+
+const user = {
+    name: "chai",
+    email : "chai@google.com"
+}
+
+const teacher = {
+    makeVideo : true
+}
+
+const TeachingSupport = {
+    isAvailable : false
+}
+
+const TASupport = {
+    makeAssignment : 'Js Assignment',
+    fullTime : true,
+    __proto__ : TeachingSupport
+}
+
+teacher.__proto__ = user
+
+//modern syntax
+
+Object.setPrototypeOf(TeachingSupport, teacher)
+
+let anotherUsername = "ChaiaurCode      "
+
+String.prototype.trueLength = function(){
+    console.log(`${this}`)
+    // console.log(`${this.name}`)
+    console.log(`True length is : ${this.trim().length}`)
+}
+
+anotherUsername.trueLength()
+"Owais".trueLength()
+"IceTea".trueLength()
